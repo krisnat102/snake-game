e48ff959f141818746a7bdb7cdde44e02607442c
Christopher <alhokolik102@gmail.com>
2024-09-12 00:12:17 +0300

    Game Manager
    Added the variables of the game manager

--- SnakeGame/SnakeGame/Scripts/GameManager.cs
+++ SnakeGame/SnakeGame/Scripts/GameManager.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeGame.Scripts
+namespace SnakeGame
 {
-    internal class GameManager
+    public class GameManager
     {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Tile[,] Grid { get; }
+        public Direction Direction { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+        
+        private readonly LinkedList<Position> snakePosition = new LinkedList<Position>();
     }
 }
